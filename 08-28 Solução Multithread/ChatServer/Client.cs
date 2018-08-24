9b77cf8effd979df41680911d7831e88b3f9f3cb
Leonardo <leobasilio@gmail.com>
2018-08-24 17:28:41 -0300

    08-28 Código do Servidor

---
+++ 08-28 Solução Multithread/ChatServer/Client.cs
@@ -0,0 +1,173 @@
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Collections.Generic;
+
+namespace ChatServer {
+
+    class Client {
+        
+        public string name {
+            get;
+            private set;
+        }
+
+        private TcpClient socket;        
+
+        private Queue<byte[]> outputQueue = new Queue<byte[]>();
+        private object outputQueueLock = new object();
+
+        private object socketWriteLock = new object();
+
+        private bool reading = false;
+
+        public delegate void NewMessageHandler(Client sender, string text);
+        public event NewMessageHandler NewMessage;
+
+        public delegate void SessionClosedHandler(Client sender);
+        public event SessionClosedHandler SessionClosed;
+
+        public Client(TcpClient socket){
+            
+            this.socket = socket;
+
+        }
+    
+        public void BeginRead(){
+
+            if(!reading){
+            
+                reading = true;
+
+                Task.Run(new Action(ReadMessages));
+
+            }
+
+        }
+
+        private void ReadMessages(){
+
+            var reader = new BinaryReader(socket.GetStream());
+
+            byte[] messageBuffer;
+
+            while(true){
+                
+                try {
+
+                    messageBuffer = reader.ReadBytes(reader.ReadInt32());
+
+                }catch(IOException){
+
+                    Disconnect();
+
+                    return;
+
+                }
+
+                var message = System.Text.Encoding.UTF8.GetString(messageBuffer);
+                var lines = message.Split('\n', 2);
+                                    
+                this.name = lines[0];
+
+                if(lines[1] != ""){
+                    
+                    NewMessage(this, lines[1]);
+
+                }else{
+                    
+                    NewMessage(this, ">> ENTROU NA SALA");
+
+                }
+
+            }
+
+        }
+
+        public void SendMessage(Client sender, string text){
+
+            if(socket == null) return;
+
+            var message = sender.name + "\n" + text;
+            
+            lock(outputQueueLock){
+
+                outputQueue.Enqueue(System.Text.Encoding.UTF8.GetBytes(message));                
+
+                if(outputQueue.Count == 1) Task.Run(new Action(SendQueueMessages));
+
+            }
+            
+        }
+
+        private void SendQueueMessages(){
+
+            while(true){
+
+                if(!Monitor.TryEnter(socketWriteLock)) return;
+
+                var writer = new BinaryWriter(socket.GetStream());
+
+                while(true){
+
+                    byte[] message;
+
+                    lock(outputQueueLock){
+
+                        if(outputQueue.Count == 0) break;
+                            
+                        message = outputQueue.Dequeue();
+
+                    }
+                    
+                    try {
+
+                        writer.Write(message.Length);
+                        writer.Write(message);
+
+                    }catch(IOException){
+
+                        Disconnect();
+
+                        break;
+
+                    }
+
+                }
+
+                Monitor.Exit(socketWriteLock);
+
+                lock(outputQueueLock){
+                    
+                    if(outputQueue.Count == 0) return;
+
+                }
+
+            }
+
+        }
+
+        private void Disconnect(){
+
+            try {                
+                socket.Close();
+            }catch{                
+            }
+
+            socket = null;
+
+            if(this.name != ""){
+
+                NewMessage(this, "<< SAIU DA SALA");
+
+                SessionClosed(this);
+
+            }
+
+        }
+
+    }
+
+}
---
+++ 08-28 Solução Multithread/ChatServer/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatServer {
+    
+    class Program {
+
+        static LinkedList<Client> clientList = new LinkedList<Client>();
+        static Object clientListLock = new Object();
+
+        static void Main(string[] args){
+            
+            var server = new TcpListener(IPAddress.Any, 8888);
+
+            while(true){
+
+                var client = new Client(server.AcceptTcpClient());
+
+                lock(clientListLock){
+                    
+                    clientList.Append(client);
+
+                }
+
+                client.NewMessage += HandleNewMessage;
+                client.SessionClosed += HandleSessionClosed;
+
+                client.BeginRead();
+
+            }
+
+        }
+
+        static void HandleNewMessage(Client sender, string text){
+
+            lock(clientListLock){
+                
+                foreach(Client client in clientList){
+
+                    if(client != sender) client.SendMessage(sender, text);
+
+                }
+
+            }
+
+        }
+
+        static void HandleSessionClosed(Client sender){
+
+            lock(clientListLock){
+
+                clientList.Remove(sender);
+
+            }
+
+        }
+
+    }
+
+}
