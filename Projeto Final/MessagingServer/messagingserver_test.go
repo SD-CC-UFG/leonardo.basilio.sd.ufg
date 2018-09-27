bbb660fd14b378d58cca4e12e585162ba73f3b2e
leandrovianna <leandro97vianna@gmail.com>
2018-09-26 21:24:14 -0300

    testes unitarios para messaging server

--- Projeto Final/MessagingServer/messagingserver_test.go
+++ Projeto Final/MessagingServer/messagingserver_test.go
@@ -10,43 +10,61 @@ import (
 	"google.golang.org/grpc"
 )
 
+func createClientConnTest(username string, parallel bool) func(t *testing.T) {
+	return func(t *testing.T) {
+		if parallel {
+			t.Parallel()
+		}
+		conn, err := grpc.Dial("localhost:7777", grpc.WithInsecure())
+		if err != nil {
+			t.Error(err)
+		}
+		defer conn.Close()
+
+		client := pb.NewMessagingServerClient(conn)
+
+		stream, err := client.TalkAndListen(context.Background())
+
+		waitc := make(chan struct{})
+		go func() {
+			for {
+				in, err := stream.Recv()
+				if err == io.EOF {
+					// read done.
+					close(waitc)
+					return
+				}
+				if err != nil {
+					t.Fatalf("Failed to receive a note : %v", err)
+				}
+
+				t.Logf("Message received from server: %v", in)
+			}
+		}()
+
+		for i := 0; i < 3; i++ {
+			chatMessage := &pb.ChatMessage{}
+			chatMessage.UserCredential = &pb.UserCredential{UserName: username}
+			chatMessage.DateTime = uint32(time.Now().Unix())
+			chatMessage.Text = &pb.TextMessage{Text: "Ola a todos!"}
+
+			if err := stream.Send(chatMessage); err != nil {
+				t.Fatalf("Failed to send a message: %v", err)
+			}
+
+			d, _ := time.ParseDuration("3s")
+			time.Sleep(d)
+		}
+		stream.CloseSend()
+		<-waitc
+	}
+}
+
 func TestClientConn(t *testing.T) {
-	conn, err := grpc.Dial("localhost:7777", grpc.WithInsecure())
-	if err != nil {
-		t.Error(err)
-	}
-	defer conn.Close()
+	t.Run("Test 1", createClientConnTest("leandro", true))
+}
 
-	client := pb.NewMessagingServerClient(conn)
-
-	stream, err := client.TalkAndListen(context.Background())
-	go func() {
-		for {
-			in, err := stream.Recv()
-			if err == io.EOF {
-				// read done.
-				return
-			}
-			if err != nil {
-				t.Fatalf("Failed to receive a note : %v", err)
-			}
-
-			t.Logf("Message received from server: %v", in)
-		}
-	}()
-
-	for i := 0; i < 10; i++ {
-		chatMessage := &pb.ChatMessage{}
-		chatMessage.UserCredential = &pb.UserCredential{UserName: "leandro"}
-		chatMessage.DateTime = uint32(time.Now().Unix())
-		chatMessage.Text = &pb.TextMessage{Text: "Ola a todos!"}
-
-		if err := stream.Send(chatMessage); err != nil {
-			t.Fatalf("Failed to send a message: %v", err)
-		}
-
-		d, _ := time.ParseDuration("5s")
-		time.Sleep(d)
-	}
-	stream.CloseSend()
+func TestTwoClientConn(t *testing.T) {
+	t.Run("Test 1", createClientConnTest("leandro", true))
+	t.Run("Test 2", createClientConnTest("joao", true))
 }
